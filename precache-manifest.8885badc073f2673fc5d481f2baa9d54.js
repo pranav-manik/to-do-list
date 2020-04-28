3ae5cd7f35f42b37adcdec1746ef6931242ccc02
Pranav <pranav.manik@gmail.com>
2020-04-28 15:16:33 -0400

    Updates

---
+++ precache-manifest.8885badc073f2673fc5d481f2baa9d54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa94b55aada167a1f0613f525bc10417",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "9e38bc8a300fef5f2643",
+    "url": "/to-do-list/static/css/2.7195bffc.chunk.css"
+  },
+  {
+    "revision": "5c53c4ab36d2652bb4d4",
+    "url": "/to-do-list/static/css/main.4a45214d.chunk.css"
+  },
+  {
+    "revision": "9e38bc8a300fef5f2643",
+    "url": "/to-do-list/static/js/2.842cb098.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/to-do-list/static/js/2.842cb098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c53c4ab36d2652bb4d4",
+    "url": "/to-do-list/static/js/main.4aa3a5c4.chunk.js"
+  },
+  {
+    "revision": "2e829da1341b6be22e90",
+    "url": "/to-do-list/static/js/runtime-main.45201a91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
